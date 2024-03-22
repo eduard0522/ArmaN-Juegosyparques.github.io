7efd4601b51af6a571d16feec1a0c2fe25b490c2
eduard villamil <villamilandrey02@gmail.com>
2024-03-21 19:57:44 -0500

    V1.0 pruba de funcionalidad

---
+++ Code/index.js
@@ -0,0 +1,47 @@
+const d = document;
+
+
+const $sliderContent = d.querySelectorAll('.slider-e-content');
+
+const $next = d.querySelector('.next');
+const $prev = d.querySelector('.prev');
+
+let index = 0;
+const mount = () =>{ $sliderContent[index].classList.add('active')};
+const unMount = () =>{ $sliderContent[index].classList.remove('active')};
+
+setInterval(() => {
+ next()
+}, 3000);
+
+
+$next.addEventListener('click' ,(e) => next() )
+$prev.addEventListener('click' , (e) => prev() )
+
+
+function next() {
+  unMount()
+  if(index >= $sliderContent.length - 1 ){
+    index = 0;
+  }else{
+    index++
+  }
+  mount()
+}
+
+
+
+ function prev () {
+ unMount()
+  if(index <= 0 ){
+    index = $sliderContent.length - 1;
+  }else{
+    index--
+  }
+  mount()
+}
+
+
+
+mount();
+
